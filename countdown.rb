58b70c73d0fd142c4a3055e5253adeef90a23304
Edward Sparks <ed.sparks@flatironschool.com>
2018-08-17 10:40:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = 0
+  while counter < number 
+    puts "#{number - counter} SECOND(S)!"
+    counter += 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  counter = 0
+  while counter < number 
+    sleep(1)
+    puts "#{number - counter} SECOND(S)!"
+    counter += 1
+  end
+  return "HAPPY NEW YEAR!"
 end
