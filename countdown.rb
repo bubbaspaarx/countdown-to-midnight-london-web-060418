dd3c8f9d0bf8570c22a9ebc5ac5c9110478c57cc
Edward Sparks <ed.sparks@flatironschool.com>
2018-08-17 10:34:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,7 +9,7 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countown_with_sleep(number)
+def countdown_with_sleep(number)
   counter = 0
   while counter <= number 
     puts "#{number - counter} SECOND(S)!"
